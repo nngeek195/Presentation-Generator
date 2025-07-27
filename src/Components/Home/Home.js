98b7f4fd597b96888be4fbc28ed00986f2ffe0f4
kaveeshasm <kaveeshasandeepanim@gmail.com>
2025-07-27 11:33:16 +0530

    Home page topic button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,8 @@ class Home extends Component {
       <div className='home_back'>
         <div className='home_mainTopic'>
             <h1>AI Presentation Generator</h1>
-            <span>Transform your ideas into stunning presentations with the power of AI</span>
+            <span>Transform your ideas into stunning presentations with the power of AI</span><br />
+            < input className='input_topic' type='text' placeholder='Enter the Topic' />
         </div>
         <div className='generate'>
             <button>Generate</button>
