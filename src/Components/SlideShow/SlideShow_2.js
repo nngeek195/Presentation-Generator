8ad277bdb9104740efe6658a02214a1e136e4848
kaveeshasm <kaveeshasandeepanim@gmail.com>
2025-07-29 21:17:08 +0530

    Login Page Update 1

--- src/Components/SlideShow/SlideShow_2.js
+++ src/Components/SlideShow/SlideShow_2.js
@@ -16,6 +16,16 @@ function SlideShow() {
     <div className='slideshow_2'>
         <Swiper
           effect={'coverflow'}
+          centeredSlides={true}
+          slidesPerView={3}
+          loop={true}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+  }}
           modules={[EffectCoverflow, Autoplay]}
           autoplay={{
             delay: 2000,       
