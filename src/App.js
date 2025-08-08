bd7675c42e5811f5dbfa4fd5043f2a4c0ee11722
Kaveesha Sandeepani <kaveeshasandeepanim@gmail.com>
2025-08-08 20:37:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import LogIn from './Components/LogIn/LogIn';
 import Home from './Components/Home/Home'
 import SignUp from './Components/Signup/SignUp';
-import Introduction from './Components/Introduction/Introduction';
+//import Introduction from './Components/Introduction/Introduction';
 import User from './Components/User/User'
 
 
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Introduction />} />
-          <Route path="/login" element={<LogIn />} />
+          //<Route path="/" element={<Introduction />} />
+          <Route path="/" element={<LogIn />} />
           <Route path="/home" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/user" element={<User />} />
