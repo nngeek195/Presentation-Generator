f077868b07cb530220cf7c78431d3d79cad6e460
kaveeshasm <kaveeshasandeepanim@gmail.com>
2025-08-29 23:30:40 +0530

    Introduction Page Logo Updated

--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Logo from './Logo.png'
 
 // --- CSS Styles ---
 // All styles are included here to keep the component self-contained.
@@ -280,7 +281,7 @@ const Header = () => (
   <header className="header">
     <div className="header-container">
       <div className="logo">
-        <span>webify</span>.me
+         <img style={{width: '256px', height: '62px', marginLeft: '-3rem'}} src={Logo} />
       </div>
       <button className="mobile-menu-btn">
         <svg xmlns="http://www.w.3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
